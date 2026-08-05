b55d23353e54fffac2a008bc3399246170ecf7ef
Lisya-myata <Lisya.Git@proton.me>
2026-08-04 14:01:30 -1000

    Finalized Disks.nix

--- disks.nix
+++ disks.nix
@@ -10,15 +10,22 @@
 						partitions = {
 							ESP = {
 								priority = 1;
+								# Name sets parition label not the file system label.
 								name = "ESP";
+								# Start and end requires absolute drive locations. You cannot start on the same unit as the end of another partition.
 								start = "1M";
 								end = "512M";
+								# Sets the partition type code. Default is filesystem and as such does not need to be set for file system partitions.
 								type = "EF00";
 								content = {
 									type = "filesystem";
-									extraArgs = [ "-n ESP" ];
+									# Disko has no label format yet so you need to pass an argument for the related mkfs.
+									# The extra arg for mkfs.vfat requires no space between label argument and the label itself.
+									extraArgs = [ "-nESP" ];
 									format = "vfat";
 									mountpoint = "/boot";
+									# Setting unix permissions often writen just as '077' because the first 0 is special bits.
+									# This allows the owner to do things with the partition but no one else has access.
 									mountOptions = [ "umask=0077" ];
 								};
 							};
@@ -26,12 +33,13 @@
 							swap = {
 								priority = 2;
 								name = "swap";
-								start = "512M";
-								end = "4G";
+								# Size uses relative amounts. 8G will partition an 8GB area after the previous partition's end.
+								size = "8G";
 								type = "8200";
 								content = {
 									type = "swap";
 									extraArgs = [ "-L swap" ];
+									# Disko will set discard and resume device with mkswap to set them for swapon you need to do that seperately.
 									discardPolicy = "both";
 									resumeDevice = true;
 								};
@@ -40,11 +48,12 @@
 							nixos = {
 								priority = 3;
 								name = "nixos";
-								start = "4G";
-								end = "100%";
+								size = "100%";
 								content = {
 									type = "btrfs";
 									extraArgs = [ "-f" "--label NixOS" ];
+									# Subvolumes are parts of a btrfs file system that are backed up separately.
+									# On the partition and file system side they are the same as normal. This also will not automatically create the directory, just tells btrfs to subvolume the directory if it exists.
 									subvolumes = {
 										"/rootfs" = {
 											mountpoint = "/";
