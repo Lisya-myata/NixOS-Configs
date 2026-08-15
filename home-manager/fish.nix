03cc3433a0067559f17c27cd5960449b73e72250
LisyaMyata <77227990+Lisya-myata@users.noreply.github.com>
2026-08-15 00:19:16 -1000

    Update fish.nix
    Same as two previous commits

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -7,7 +7,7 @@
 					cd = "z";
 					ls = "eza -l -a";
 					shutdown = "systemctl poweroff";
-					nix-rebuild = "sudo nixos-rebuild switch --flake ~/.dotfiles#nixosvm";
+					nix-rebuild = "sudo nixos-rebuild switch --flake ~/.dotfiles#v-nixos";
 					nix-update = "sudo nix flake update --flake ~/.dotfiles";
 					nix-garbage-collect = "sudo nix-collect-garbage -d";
 				};
